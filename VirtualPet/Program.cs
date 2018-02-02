cdcd59f377ac503e9d0c5a6884dec32fa7999a3b
C90Gray <cmusic.gray@gmail.com>
2018-02-02 11:12:21 -0500

    added classes and main menu

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -16,21 +16,25 @@ namespace VirtualPet
             Console.WriteLine("For Melvin the Monkey, please enter 2");
             Console.WriteLine("For Cletus the Camel, please enter 3");
             int name = int.Parse(Console.ReadLine());
-            string petName;
+            string petName = "";
+            string firstName = "";
             switch (name)
             {
                 case 1:
-                    petName = "LarryLion"; 
+                    petName = "Larry the Lion";
+                    firstName = "Larry";
                     Console.WriteLine("You picked Larry the Lion! He's a wild beast... and I ain't lion...");
                     Console.WriteLine("");
                     break;
                 case 2:
-                    petName = "MelvinMonkey";
+                    petName = "Melvin the Monkey";
+                    firstName = "Melvin";
                     Console.WriteLine("You picked Melvin the Monkey! You're bananas!");
                     Console.WriteLine("");
                     break;
                 case 3:
-                    petName = "CletusCamel";
+                    petName = "Cletus the Camel";
+                    firstName = "Cletus";
                     Console.WriteLine("You picked Cletus the Camel! Don't ever dessert him.");
                     Console.WriteLine("");
                     break;
@@ -40,7 +44,25 @@ namespace VirtualPet
                     
             }
 
-            
+            CletusCamel petCletusCamel = new CletusCamel(70, 65, 10, 5);
+            MelvinMonkey petMelvinMonkey = new MelvinMonkey(70, 65, 10, 5);
+            LarryLion petLarryLion = new LarryLion(70, 65, 10, 5);
+
+            int action;
+            do
+            {
+                Console.WriteLine("What would you like to do with " + petName + " ?\nPlease enter a number from the following list.\n\n");
+                Console.WriteLine("1: Feed " + firstName);
+                Console.WriteLine("2: Give " + firstName + " water");
+                Console.WriteLine("3: Play with " + firstName);
+                Console.WriteLine("4: Let " + firstName + " catch up on some sleep");
+                Console.WriteLine("5: Take " + firstName + " to go potty");
+                Console.WriteLine("6: Do nothing");
+                Console.WriteLine("7: Exit the game");
+                action = int.Parse(Console.ReadLine());
+            } while (action != 7);
+
+
         }
     }
 }
