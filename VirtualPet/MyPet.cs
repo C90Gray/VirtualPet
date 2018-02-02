baa47dd59dbe098bcbaf63e6a184cbbdefe2546d
C90Gray <cmusic.gray@gmail.com>
2018-02-02 13:01:43 -0500

    deleted classes, renamed class, finished writing class and methods.

---
+++ VirtualPet/MyPet.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class MyPet
+    {
+        private bool isHungry;
+        private int hungerLevel;
+        private bool isThirsty;
+        private int thirstLevel;
+        private bool isTired;
+        private int tiredness;
+        private bool isBored;
+        private int bordomLevel;
+        private bool hasToPotty;
+
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+        
+        public int HungerLevel
+        {
+            get { return this.hungerLevel; }
+            set { this.hungerLevel = value; }
+        }
+
+        public bool IsThirsty
+        {
+            get { return this.isThirsty; }
+            set { this.isThirsty = value; }
+        }
+
+        public int ThirstLevel
+        {
+            get { return this.thirstLevel; }
+            set { this.thirstLevel = value; }
+        }
+
+        public bool IsTired
+        {
+            get { return this.isTired; }
+            set { this.isTired = value; }
+        }
+        
+        public int Tiredness
+        {
+            get { return this.tiredness; }
+            set { this.tiredness = value; }
+        }
+
+        public bool IsBored
+        {
+            get { return this.isBored; }
+            set { this.isBored = value; }
+        }
+
+        public int BordomLevel
+        {
+            get { return this.bordomLevel; }
+            set { this.bordomLevel = value; }
+        }
+
+        public bool HasToPotty
+        {
+            get { return this.hasToPotty; }
+            set { this.hasToPotty = value; }
+        }
+
+        public MyPet()
+        {
+
+        }
+
+
+        public MyPet(int hungerLevel, int thirstLevel, int tiredness, int bordomLevel)
+        {
+            this.hungerLevel = hungerLevel;
+            this.thirstLevel = thirstLevel;
+            this.tiredness = tiredness;
+            this.bordomLevel = bordomLevel;
+        }
+
+        public string FeedMe()
+        {
+            if (hungerLevel > 50)
+            {
+                IsHungry = false;
+                hungerLevel -= 10;
+                return "has been fed. \n";
+            }
+            else
+            {
+                hasToPotty = true;
+                return "isn't hungry. \n";
+            }
+        }
+
+        public string Drink()
+        {
+            if (thirstLevel > 50)
+            {
+                isThirsty = false;
+                hasToPotty = true;
+                thirstLevel = 0;
+                return "'s thirst was quenched. \n";
+            }
+            else
+            {
+                return "isn't thirsty right now.";
+            }
+        }
+
+        public void Play()
+        {
+            bordomLevel -= 25;
+            hungerLevel += 15;
+            thirstLevel += 25;
+        }
+
+        public void Sleep()
+        {
+            if (tiredness > 25)
+            {
+                bordomLevel += 10;
+                hungerLevel += 20;
+                thirstLevel += 10;
+                tiredness = 0;
+            }
+            else
+            {
+                Console.WriteLine("He isn't tired right now");
+            }
+        }
+
+        public void Potty()
+        {
+            hungerLevel += 15;
+            thirstLevel += 10;
+        }
+
+        public void Tick()
+        {
+            Random r = new Random();
+
+            hungerLevel = hungerLevel + r.Next(-10, 10);
+            thirstLevel = thirstLevel + r.Next(-3, 3);
+            tiredness = tiredness + r.Next(-30, 30);
+            bordomLevel = bordomLevel = r.Next(-15, 15);
+            if (hungerLevel > 50)
+            {
+                isHungry = true;
+            }
+            else
+            {
+                isHungry = false;
+                hasToPotty = true
+            }
+
+            if (thirstLevel > 50)
+            {
+                isThirsty = true;
+            }
+            else
+            {
+                isThirsty = false;
+                hasToPotty = true;
+            }
+
+            if (bordomLevel > 50)
+            {
+                isBored = true;
+            }
+            else
+            {
+                isBored = false;
+            }
+
+            if (tiredness > 50)
+            {
+                isTired = true;
+            }
+            else
+            {
+                isTired = false;
+            }
+
+            Console.WriteLine("It is " + isHungry + " that your pet is hungry. \nHunger Level: " + hungerLevel);
+            Console.WriteLine("It is " + IsThirsty + " that your pet is thirsty. \nThirst Level: " + thirstLevel);
+            Console.WriteLine("It is " + isTired + " that your pet is tired. \nTiredness: " + tiredness);
+            Console.WriteLine("It is " + isBored + " that your pet is bored. \nBordom Level: " + bordomLevel);
+            Console.WriteLine("It is " + hasToPotty + " that your pet has to go potty.");
+        }
+    }
+}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,57 +10,63 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Virtual Pet! \n\nTo begin you will choose a pet.");
-            Console.WriteLine("\n\nWhich pet would you like to adopt?");
-            Console.WriteLine("For Larry the Lion, please enter 1");
-            Console.WriteLine("For Melvin the Monkey, please enter 2");
-            Console.WriteLine("For Cletus the Camel, please enter 3");
-            int name = int.Parse(Console.ReadLine());
+            int action = 0;
             string petName = "";
             string firstName = "";
-            switch (name)
-            {
-                case 1:
-                    petName = "Larry the Lion";
-                    firstName = "Larry";
-                    Console.WriteLine("You picked Larry the Lion! He's a wild beast... and I ain't lion...");
-                    Console.WriteLine("");
-                    break;
-                case 2:
-                    petName = "Melvin the Monkey";
-                    firstName = "Melvin";
-                    Console.WriteLine("You picked Melvin the Monkey! You're bananas!");
-                    Console.WriteLine("");
-                    break;
-                case 3:
-                    petName = "Cletus the Camel";
-                    firstName = "Cletus";
-                    Console.WriteLine("You picked Cletus the Camel! Don't ever dessert him.");
-                    Console.WriteLine("");
-                    break;
-                default:
-                    Console.WriteLine("You stink at following directions. Please close the program and try again.");
-                    break;
-                    
-            }
-
-            CletusCamel petCletusCamel = new CletusCamel(70, 65, 10, 5);
-            MelvinMonkey petMelvinMonkey = new MelvinMonkey(70, 65, 10, 5);
-            LarryLion petLarryLion = new LarryLion(70, 65, 10, 5);
-
-            int action;
             do
             {
-                Console.WriteLine("What would you like to do with " + petName + " ?\nPlease enter a number from the following list.\n\n");
-                Console.WriteLine("1: Feed " + firstName);
-                Console.WriteLine("2: Give " + firstName + " water");
-                Console.WriteLine("3: Play with " + firstName);
-                Console.WriteLine("4: Let " + firstName + " catch up on some sleep");
-                Console.WriteLine("5: Take " + firstName + " to go potty");
-                Console.WriteLine("6: Do nothing");
-                Console.WriteLine("7: Exit the game");
-                action = int.Parse(Console.ReadLine());
-            } while (action != 7);
+                Console.WriteLine("Welcome to Virtual Pet! \n\nTo begin you will choose a pet.");
+                Console.WriteLine("\n\nWhich pet would you like to adopt?");
+                Console.WriteLine("For Larry the Lion, please enter 1");
+                Console.WriteLine("For Melvin the Monkey, please enter 2");
+                Console.WriteLine("For Cletus the Camel, please enter 3");
+                int name = int.Parse(Console.ReadLine());
+
+                switch (name)
+                {
+                    case 1:
+                        petName = "Larry the Lion";
+                        firstName = "Larry";
+                        Console.WriteLine("You picked Larry the Lion! He's a wild beast... and I ain't lion...");
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        petName = "Melvin the Monkey";
+                        firstName = "Melvin";
+                        Console.WriteLine("You picked Melvin the Monkey! You're bananas!");
+                        Console.WriteLine("");
+                        break;
+                    case 3:
+                        petName = "Cletus the Camel";
+                        firstName = "Cletus";
+                        Console.WriteLine("You picked Cletus the Camel! Don't ever dessert him.");
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("You stink at following directions. Please try again. \nThe program will now close.");
+                        return;
+
+                }
+
+
+
+                MyPet petMe = new MyPet(70, 65, 10, 5);               
+
+                do
+                {
+                    Console.WriteLine("What would you like to do with " + petName + " ?\nPlease enter a number from the following list.\n\n");
+                    Console.WriteLine("1: Feed " + firstName);
+                    Console.WriteLine("2: Give " + firstName + " water");
+                    Console.WriteLine("3: Play with " + firstName);
+                    Console.WriteLine("4: Let " + firstName + " catch up on some sleep");
+                    Console.WriteLine("5: Take " + firstName + " to go potty");
+                    Console.WriteLine("6: Do nothing");
+                    Console.WriteLine("7: Exit the game");
+                    Console.WriteLine("0: Pick a new pet and restart the game");
+                    action = int.Parse(Console.ReadLine());
+
+                } while ((action != 7) && (action != 0 ));
+            } while (action == 0);
 
 
         }
